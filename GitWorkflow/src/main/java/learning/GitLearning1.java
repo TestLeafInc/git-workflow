d0cd2f0ab822ffe401451942d8ad6617e824bb93
Testleaf <babu@testleaf.com>
2023-09-02 10:13:43 +0530

    This is first baseline code with GIT Learning

---
+++ GitWorkflow/src/main/java/learning/GitLearning1.java
@@ -0,0 +1,11 @@
+package learning;
+
+public class GitLearning1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("I am the first code to go to the repo !");
+	}
+
+}
